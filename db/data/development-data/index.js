f763bd0ba083fc256bb5df742c9f3fa24cf8beeb
mbaxret2 <emily.turner@manchester.ac.uk>
2019-12-10 10:20:25 +0000

    initial

---
+++ db/data/development-data/index.js
@@ -0,0 +1,4 @@
+const articleData = require('./articles.js')
+const commentData = require('./comments.js')
+const topicData = require('./topics.js')
+const userData = require('./users.js')
---
+++ db/data/index.js
@@ -0,0 +1,11 @@
+const ENV = process.env.NODE_ENV || 'development';
+
+const test = require('./test-data/index.js');
+const development = require('./development-data.js');
+
+const data = {
+  test,
+  development
+  }
+
+module.exports = data[ENV];
